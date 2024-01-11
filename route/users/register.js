2f14cde337cb4d7dca921523bfd6d611fd80c9ad
scotty974 <fabienetheve@hotmail.com>
2024-01-11 12:39:55 +0100

    refactor and build jwt sign

--- route/users/register.js
+++ route/users/register.js
@@ -1,6 +1,7 @@
 import express from "express";
 import { PrismaClient } from "@prisma/client";
 import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
 import uservalidation from "./userFormValidator.js";
 import createHttpError from "http-errors";
 import generatorUsername from "./generatorUsername.js";
@@ -9,6 +10,8 @@ const prisma = new PrismaClient();
 
 router.post("/register", async (req, res, next) => {
   let registerData;
+  const currentDate = new Date();
+  const isoDate = currentDate.toISOString();
   try {
     // on recupere les données du form
     registerData = uservalidation.parse(req.body);
@@ -24,19 +27,35 @@ router.post("/register", async (req, res, next) => {
       id: registerData.id,
     },
   });
-  if(user){
-    return next(createHttpError(400, "Un compte existe déjà avec ce compte"))
-  }else{
-    const hashedPassword = await bcrypt.hash(registerData.password, 10)
-    await prisma.users.create({
+  if (user) {
+    return next(createHttpError(400, "Un compte existe déjà avec ce compte"));
+  } else {
+    const hashedPassword = await bcrypt.hash(registerData.password, 10);
+    await prisma.users
+      .create({
         data: {
-            email : registerData.email, 
-            password : hashedPassword, 
-            username : generatorUsername()
-            
-        }
-    })
+          email: registerData.email,
+          password: hashedPassword,
+          username: generatorUsername(),
+          created_at: isoDate,
+          profile_picture: "none",
+          point: 0,
+        },
+      })
+      .then((user) => {
+        res.json({
+          token: jwt.sign(
+            {
+              id: user.id,
+              email: user.email,
+              username: user.username,
+            },
+            process.env["JWT_KEY"],
+            {
+              expiresIn: 86400,
+            }
+          ),
+        });
+      });
   }
-
-
 });
