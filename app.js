8468d81d0cd8509f09c93a05cd6b4cb7f271c913
scotty974 <fabienetheve@hotmail.com>
2024-01-11 11:23:06 +0100

    add folder route

--- app.js
+++ app.js
@@ -1,11 +1,11 @@
 import express from 'express'
 
 const app = express()
+app.use(express.json())
+app.use(express.urlencoded({extended:true}))
 const port = 3000
 
-app.get('/', (req,res)=>{
-    res.send('Hello world')
-})
+
 
 app.listen(port, ()=>{
     console.log(`App listening on port : ${port}`)
